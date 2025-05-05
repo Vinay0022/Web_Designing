1f47da719104f3b86d345551fcce3cb086cb1f7f
Vinay022 <vinu031995@gmail.com>
2025-05-06 00:12:25 +0530

    first task sort prompt arrays using sort function done

--- web_designing_25_03/Javascript/day19/tasks/arrayPromt.js
+++ web_designing_25_03/Javascript/day19/tasks/arrayPromt.js
@@ -2,13 +2,13 @@ let arrS = prompt("Enter array values: ");
 
 document.write(arrS);
 console.log(arrS);
+// split returns string array
 let arrN = arrS.split(",");
 
-arrN.sort(
-  (function (p, q) {
-    return p - q;
-  })
-);
+arrN.sort(function (p, q) {
+  return p - q;
+});
 document.write(`<br>`);
 document.write(arrN);
 console.log(arrN);
+console.log(typeof arrN);
